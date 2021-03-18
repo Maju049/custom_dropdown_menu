f554ff8f1290cab6802b3a14458960a5a8db18db
Maju049 <64065807+Maju049@users.noreply.github.com>
2021-03-18 07:30:51 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# simple account menu (Dropdown) made in flutter.
+
+Created simple account menu design by [Zach Roszczewski](https://dribbble.com/shots/893233-Simple-Account-Menu-Dropdown)
+
+![Demo](https://github.com/shubham030/simple_account_menu/blob/master/demo.gif)
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:simple_account_menu/simple_account_menu.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          color: Color(0xFFF7A8499),
+          child: Center(
+            child: Container(
+                child: SimpleAccountMenu(
+              icons: [
+                Icon(Icons.person),
+                Icon(Icons.settings),
+                Icon(Icons.credit_card),
+              ],
+              iconColor: Colors.white,
+              onChange: (index) {
+                print(index);
+              },
+            )),
+          ),
+        ),
+      ),
+    );
+  }
+}
